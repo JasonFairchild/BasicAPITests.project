4fd3d27d14855fa1450fe3cca3e1b64e4ca4e1dc
JasonFairchild <ammonjasonfairchild@gmail.com>
2019-10-09 16:39:35 -0600

    Add test for Authors GET by IDBook - struggled with the changing bookIDs and response object a fair bit

--- src/test/java/TestAuthorsAPI.java
+++ src/test/java/TestAuthorsAPI.java
@@ -1,12 +1,24 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestAuthorsAPI {
+    public int bookID = 2;
+    public static Response response;
 //    Set the base URI before each test is run
     @Before
     public void setUp(){
@@ -62,11 +74,31 @@ public class TestAuthorsAPI {
 
     /**
      * Test the Authors GET by Book ID endpoint
-     * The response body should contain each Author with the associated Book ID
+     * The response body should contain each Author with the specified Book ID
+     * The Book ID constantly changes, so I just test that each response Book ID matches the expected Book ID
      */
     @Test
     public void testAuthorsGetIDBook()
     {
+        response =
+            given().
+                pathParam("IDBook", bookID).
+            when().
+                get("/authors/books/{IDBook}").
+            then().
+                contentType(ContentType.JSON).
+                log().body().
+                extract().response();
+        String jsonAsString = response.asString();
+        System.out.println(jsonAsString);
 
+        // Put all bookIDs into a list
+        List<Integer> bookIDs = response.path("IDBook");
+
+        // Check that each IDBook is equal to 2
+        for (int ID : bookIDs)
+        {
+            assertThat(ID, equalTo(bookID));
+        }
     }
 }
